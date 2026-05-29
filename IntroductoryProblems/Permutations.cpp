c0a57cbd5c67ee59d8a8dcb12e1d091e212d0649
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-05-28 23:49:12 -0500

    Add files via upload

---
+++ IntroductoryProblems/Permutations.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+
+    if (n == 2 || n == 3) {
+        cout << "NO SOLUTION" << endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 1) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
